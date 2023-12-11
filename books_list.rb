7e18c4c5d6f0d69cb93d3b488f9ea1fa67942b8d
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-12-11 16:05:17 -0600

    Refactor: add method to select book.

--- books_list.rb
+++ books_list.rb
@@ -21,4 +21,13 @@ class BooksList
     puts 'Book created successfully'
     puts
   end
+
+  def select_book
+    @interface.print_message('Select a book from the following list by number')
+    @books.each_with_index do |book, index|
+      @interface.print_message("#{index}) Title: \"#{book.title}\", Author: #{book.author}")
+    end
+    book_index = @interface.input.to_i
+    @books[book_index]
+  end
 end

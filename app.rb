af8e2f627060812c02d7159b70c3e388b85bd518
jcmunav63 <jcmunav63@gmail.com>
2023-12-11 20:02:39 -0600

    Refactor: Edit Main & App files to refactor main menu.

--- app.rb
+++ app.rb
@@ -17,13 +17,13 @@ class App
     @rentals_list = RentalsList.new(@my_ui, @books_list, @people_list)
   end
 
-  def start
-    loop do
-      @my_ui.show_console
-      choice = @my_ui.gets_option
-      handle_option(choice)
-    end
-  end
+  # def start
+  #   loop do
+  #     @my_ui.show_console
+  #     choice = @my_ui.gets_option
+  #     handle_option(choice)
+  #   end
+  # end
 
   def list_all_books
     @books_list.list_all_books
@@ -49,24 +49,24 @@ class App
     @people_list.list_all_people
   end
 
-  def handle_option(choice)
-    options = {
-      '1' => method(:list_all_books),
-      '2' => method(:list_all_people),
-      '3' => method(:create_person),
-      '4' => method(:create_book),
-      '5' => method(:create_rental),
-      '6' => method(:list_rentals_for_person_id),
-      '7' => proc {
-               puts 'Thank you for using this app!'
-               exit
-             }
-    }
+  # def handle_option(choice)
+  #   options = {
+  #     '1' => method(:list_all_books),
+  #     '2' => method(:list_all_people),
+  #     '3' => method(:create_person),
+  #     '4' => method(:create_book),
+  #     '5' => method(:create_rental),
+  #     '6' => method(:list_rentals_for_person_id),
+  #     '7' => proc {
+  #              puts 'Thank you for using this app!'
+  #              exit
+  #            }
+  #   }
 
-    if options[choice]
-      options[choice].call
-    else
-      puts 'Invalid option'
-    end
-  end
+  #   if options[choice]
+  #     options[choice].call
+  #   else
+  #     puts 'Invalid option'
+  #   end
+  # end
 end

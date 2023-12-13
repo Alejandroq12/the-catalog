0aa13df096d33609572834b1330bc89eb302031c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-12-13 09:05:57 -0600

    Test: add tests for nameable

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,12 @@
+# spec/nameable_spec.rb
+require_relative '../nameable'
+
+describe Nameable do
+  context 'when correct_name is called on a Nameable instance' do
+    nameable = Nameable.new
+
+    it 'raises NotImplementedError' do
+      expect { nameable.correct_name }.to raise_error(NotImplementedError)
+    end
+  end
+end
